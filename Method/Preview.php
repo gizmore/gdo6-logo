a358b7b7b9506d36a167f6f59a99a05dde2d9f0e
gizmore <gizmore@wechall.net>
2017-12-31 02:13:10 +0100

    6.07_logo

--- Method/Preview.php
+++ Method/Preview.php
@@ -3,6 +3,7 @@ namespace GDO\Logo\Method;
 use GDO\Core\Method;
 use GDO\File\Method\GetFile;
 use GDO\Logo\Module_Logo;
+use GDO\File\FileUtil;
 
 final class Preview extends Method
 {
@@ -10,6 +11,9 @@ final class Preview extends Method
 	{
 		if (!($id = Module_Logo::instance()->cfgLogoId()))
 		{
+			header('Content-Type: image/png');
+			echo file_get_contents(Module_Logo::instance()->filePath('img/gdo6-logo.png'));
+			die();
 		}
 		return GetFile::make()->executeWithId($id);
 	}
